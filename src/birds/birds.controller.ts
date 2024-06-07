a07e220f1104a48f84969076f7c78f1747df0382
ElioFezollari <fezollarielio@gmail.com>
2024-06-06 20:19:59 -0400

    Added birds controller and services, connected the module with mongo and the bird schema

--- src/birds/birds.controller.ts
+++ src/birds/birds.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { BirdsService } from './birds.service';
+import { CreateBirdDto } from './birds.model';
 
 @Controller('birds')
-export class BirdsController {}
+export class BirdsController {
+    constructor(private readonly birdsService:BirdsService){}
+
+    @Get()
+    async getAllCats(){
+        return await this.birdsService.getAllBirds();
+    }
+
+    @Get(':species')
+    async getCatBySpecies(@Param('species') speciesName:string){
+        return await this.birdsService.getBirdBySpecies(speciesName)
+    }
+    @Post()
+    @UsePipes(new ValidationPipe())
+    async addBird(@Body() birdData: CreateBirdDto) {
+        return await this.birdsService.addBird(birdData);
+    }
+    
+    @Patch(':id')
+    async updateBird(@Param('id') birdId:string,@Body() birdData:CreateBirdDto){
+        return await this.birdsService.updateBird(birdId,birdData)
+    }
+
+    @Delete(':id')
+    async deleteBird(@Param('id') birdId:string){
+        return await this.birdsService.deleteBird(birdId);
+    }
+}
--- src/birds/birds.module.ts
+++ src/birds/birds.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BirdsController } from './birds.controller';
 import { BirdsService } from './birds.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BirdSchema } from './birds.model';
 
 @Module({
+  imports:[MongooseModule.forFeature([{name:'Bird',schema:BirdSchema}])]
   controllers: [BirdsController],
   providers: [BirdsService]
 })
